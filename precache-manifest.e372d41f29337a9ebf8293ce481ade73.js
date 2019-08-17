12dc109564bc457a0ee4c9276b56561f39db8317
Ahmet Kuslular <ahmet.kuslular@iyzico.com>
2019-08-17 18:19:26 +0300

    Updates

---
+++ precache-manifest.e372d41f29337a9ebf8293ce481ade73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e582dc4b8a2a94911dc379a0d152f35",
+    "url": "/react-2048/index.html"
+  },
+  {
+    "revision": "035604d934d05f490741",
+    "url": "/react-2048/static/css/main.f647e8df.chunk.css"
+  },
+  {
+    "revision": "e8a6361b3fa9b9599168",
+    "url": "/react-2048/static/js/2.b0639382.chunk.js"
+  },
+  {
+    "revision": "035604d934d05f490741",
+    "url": "/react-2048/static/js/main.4e1c6543.chunk.js"
+  },
+  {
+    "revision": "986df1d929d338652909",
+    "url": "/react-2048/static/js/runtime~main.972aab94.js"
+  }
+]);
